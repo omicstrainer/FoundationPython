92f971b994d70ab70e6624059ef2413f71a0aca8
drabhishekkumar <draabhishekkumar@gmail.com>
2020-11-22 23:21:15 +0530

    Update list-example.py

--- List-Python/list-example.py
+++ List-Python/list-example.py
@@ -17,20 +17,35 @@ print(vertebrates[2])
 ## print 4th element of vertebrates
 print ("4th element of vertebrates:")
 print(vertebrates[3])
+
+## print 5th element of vertebrates
 print ("5th element of vertebrates:")
 print(vertebrates[4])
+
+## print 6th element of vertebrates
 print ("6th element of vertebrates:")
 print(vertebrates[5])
+
+## print 7th element of vertebrates
 print ("7th element of vertebrates:")
 print(vertebrates[6])
+
+## print 8th element of vertebrates
 print ("8th element of vertebrates:")
 print(vertebrates[7])
+
+## print 9th element of vertebrates
 print ("9th element of vertebrates:")
 print(vertebrates[8])
+
+## print -1 element of vertebrates
 print ("-1 element of vertebrates:")
 print(vertebrates[-1])
+
+## print -5 element of vertebrates
 print ("-5 element of vertebrates:")
-print(vertebrates[-5])
+print(vertebrates[-5]
+)
 print ("-9 element of vertebrates:")
 print(vertebrates[-9])
 print ("last element of vertebrates:")
