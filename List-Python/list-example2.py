0fc7487d14c1e796dd39039cc750823cca69e289
drabhishekkumar <draabhishekkumar@gmail.com>
2020-11-23 01:15:04 +0530

    Update list-example2.py

--- List-Python/list-example2.py
+++ List-Python/list-example2.py
@@ -8,7 +8,6 @@ Created on Wed Oct  7 13:22:12 2020
 ## Create a list of European countries
 Europe = ["Russia", "Ukraine", "France", "Spain", "Sweden", "Norway", "Germany", "Finland", "Poland", "Italy", "Romania", "Belarus", "Kazakhstan", "Greece", "Bulgaria", "Iceland", "Hungary", "Portugal", "Austria", "Czechia", "Serbia", "Ireland", "Lithuania", "Latvia", "Croatia", "Bosnia-Herzegovina", "Slovakia", "Estonia", "Denmark", "Switzerland", "Netherlands", "Moldova", "Belgium", "Armenia", "Albania", "North-Macedonia", "Turkey", "Slovenia", "Montenegro", "Kosovo", "Cyprus", "Azerbaijan", "Luxembourg", "Georgia", "Andorra", "Malta", "Liechtenstein", "San_Marino", "Monaco", "Vatican_City", "United_Kingdom"]
 
-
 ## print list Europe
 print(Europe)
 
@@ -26,15 +25,35 @@ print("Total length: ", length)
 
 ## print 1st element of the list Europe
 print(Europe[0])
+## print 11th element of the list Europe
 print(Europe[10])
+
+## print 21th element of the list Europe
 print(Europe[20])
+
+## print 31th element of the list Europe
 print(Europe[30])
+
+## print 41th element of the list Europe
 print(Europe[40])
+
+## print 51th element of the list Europe
 print(Europe[50])
 
+## print 1st element of the list Europe using negative value
 print(Europe[-51])
+
+## print -41 element of the list Europe using negative value
 print(Europe[-41])
+
+## print -31 element of the list Europe using negative value
 print(Europe[-31])
+
+## print -21 element of the list Europe using negative value
 print(Europe[-21])
+
+## print -11 element of the list Europe using negative value
 print(Europe[-11])
+
+## print -1 element of the list Europe using negative value
 print(Europe[-1])
