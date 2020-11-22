278f98d6433204d18a70b5be2e1cc7e00723ebea
drabhishekkumar <draabhishekkumar@gmail.com>
2020-11-23 00:27:52 +0530

    Update list-example2.py

--- List-Python/list-example2.py
+++ List-Python/list-example2.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct  7 13:22:12 2020
+@author: Abhishek Kumar
+@Purpose: Training 
+"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  7 13:22:12 2020
 
 @author: Abhishek Kumar
 """
