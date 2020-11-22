fb6f553d5643518e006a38419d2ad0fe89086469
drabhishekkumar <draabhishekkumar@gmail.com>
2020-11-23 01:09:02 +0530

    list europe

--- List-Python/list-example2.py
+++ List-Python/list-example2.py
@@ -3,12 +3,38 @@
 """
 Created on Wed Oct  7 13:22:12 2020
 @author: Abhishek Kumar
-@Purpose: Training 
+@Purpose: Training
 """
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  7 13:22:12 2020
+## Create a list of European countries
+Europe = ["Russia", "Ukraine", "France", "Spain", "Sweden", "Norway", "Germany", "Finland", "Poland", "Italy", "Romania", "Belarus", "Kazakhstan", "Greece", "Bulgaria", "Iceland", "Hungary", "Portugal", "Austria", "Czechia", "Serbia", "Ireland", "Lithuania", "Latvia", "Croatia", "Bosnia-Herzegovina", "Slovakia", "Estonia", "Denmark", "Switzerland", "Netherlands", "Moldova", "Belgium", "Armenia", "Albania", "North-Macedonia", "Turkey", "Slovenia", "Montenegro", "Kosovo", "Cyprus", "Azerbaijan", "Luxembourg", "Georgia", "Andorra", "Malta", "Liechtenstein", "San_Marino", "Monaco", "Vatican_City", "United_Kingdom"]
 
-@author: Abhishek Kumar
-"""
+
+## print list Europe
+print(Europe)
+
+## sort list Europe
+Europe.sort()
+
+## print list Europe
+print(Europe)
+
+## Measure the length of list Europe
+length = len(Europe)
+
+## print list Europe
+print("Total length: ", length)
+
+## print 1st element of the list Europe
+print(Europe[0])
+print(Europe[10])
+print(Europe[20])
+print(Europe[30])
+print(Europe[40])
+print(Europe[50])
+
+print(Europe[-51])
+print(Europe[-41])
+print(Europe[-31])
+print(Europe[-21])
+print(Europe[-11])
+print(Europe[-1])
