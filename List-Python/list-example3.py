3ff77125a69d442067b77cd74c7392dd2230ffb7
drabhishekkumar <draabhishekkumar@gmail.com>
2020-11-23 01:37:30 +0530

    Update list-example3.py

--- List-Python/list-example3.py
+++ List-Python/list-example3.py
@@ -5,3 +5,20 @@ Created on Wed Oct  7 13:22:12 2020
 
 @author: Abhishek Kumar
 """
+num =[20]*10
+
+print(num)
+
+print(num[0:10])
+print(num[0:1])
+print(num[0:2])
+print(num[0:3])
+print(num[0:4])
+print(num[0:5])
+
+print(num[0:-1])
+print(num[0:-2])
+print(num[0:-3])
+print(num[2:])
+print(num[4:])
+print(num[6:])
