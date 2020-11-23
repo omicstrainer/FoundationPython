ec623103e20b8726a745824f6c007e51b49bd954
drabhishekkumar <draabhishekkumar@gmail.com>
2020-11-23 23:51:19 +0530

    loops

---
+++ Loop-Python/loop-example2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 14-Oct-2020
+@author: Abhishek Kumar
+"""
+for i in range(2,9):
+ print(i)
---
+++ Loop-Python/loop-example3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed 14-Oct-2020
+@author: Abhishek Kumar
+"""
+for i in range(15, 45, 3):
+ print(i)
