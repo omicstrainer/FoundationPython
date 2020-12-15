81b7285cd94ac01545f279cad90357ca2d82fabc
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 22:47:35 +0530

    Update list-example7.py

--- List-Python/list-example7.py
+++ List-Python/list-example7.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  7 13:22:12 2020
-
+Created on TUE 15/12/2020 22:43
+File name: list-example.py
 @author: Abhishek Kumar
+Purpose - Example Shallow copy - assignment of one list to another and then changes in one list will be seen in other also. 
 """
+#Create list1
+list1 = [11, 22, 33, 44, 55, 66, 77]
+print (list1)
+list2 = list1
