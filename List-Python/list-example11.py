5fbade3d6a2f9d752cd472d9ba43f99193815e46
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-16 00:13:31 +0530

    Create list-example11.py

---
+++ List-Python/list-example11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on TUE 16/12/2020 00:11
+File name: list-example11.py
+@author: Abhishek Kumar
+Purpose - Test2
+"""
+# Create list of six odd numbers 
+Odd = ['1', '3', '5', '7', '9']
+
+
