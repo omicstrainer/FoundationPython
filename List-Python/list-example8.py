3ffb53c640b0c1337a6fc1e5197fb257a4e38c94
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 23:08:30 +0530

    Update list-example8.py

--- List-Python/list-example8.py
+++ List-Python/list-example8.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  7 13:22:12 2020
-
+Created on TUE 15/12/2020 22:43
+File name: list-example8.py
 @author: Abhishek Kumar
+Purpose - Example of Deep copy - assignment of one list to another and then changes in one list will be NOT seen in other also. 
 """
+#Create list1
+list1 = [11, 22, 33, 44, 55, 66, 77]
+#print list1
+print (list1)
+
+#List assignment of  list1 to list2
+list 
+list2 = list1
+
+#print list2
+print (list2)
+
+list1[0]=110
+
+#print list1
+print (list1)
+
+#print list2
+print (list2)
+
+#Removing 44 from list2
+list2.pop(3)
+
+
+#print list1
+print (list1)
+
+#print list2
+print (list2)
+
+#Reverse list1
+list1.reverse()
+
+#print list1
+print (list1)
+
+#print list2
+print (list2)
+
+#Sort list2
+list2.sort()
+
+#print list1
+print (list1)
+
+#print list2
+print (list2)
