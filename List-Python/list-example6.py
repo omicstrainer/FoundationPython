de93153ebb53fdf4679f50381618409b22da46a1
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 22:20:56 +0530

    Update list-example6.py

--- List-Python/list-example6.py
+++ List-Python/list-example6.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  7 13:22:12 2020
-
+Created on 15/12 Tuesday 22:06:12 2020
+Filename: list-example5.py
 @author: Abhishek Kumar
 """
+# Three elements - EU Country
+Europe = ["Germany", "Austria", "UK"]
+# Four elements - Vertebrates
+Vertebrates = ["Cow", "Chichen", "Fugu", "Xenopus"]
+# Combining two lists
+Vert_Euro = Vertebrates + Europe
+# Printing Vert_Euro
+print(Vert_Euro)
+#Sorting Vert_Euro
+Vert_Euro.sort()
+# Printing Sorted Vert_Euro
+print(Vert_Euro)
+#Length  Vert_Euro
+Length = len(Vert_Euro)
+print(Length)
+# max Vert_Euro
+Maxi = max(Vert_Euro)
+print(Maxi)
+# min Vert_Euro
+Mini = min(Vert_Euro)
+print(Mini)
