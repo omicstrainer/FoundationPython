f805cbb2388a770a3d50bec68e6edc68b4aaa860
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 23:30:13 +0530

    Update list-example9.py

--- List-Python/list-example9.py
+++ List-Python/list-example9.py
@@ -4,5 +4,9 @@
 Created on TUE 15/12/2020 22:43
 File name: list-example8.py
 @author: Abhishek Kumar
-Purpose - Example of Deep copy - assignment of one list to another and then changes in one list will be NOT seen in other also.
+Purpose - Example of Difference in Deletion
 """
+
+## Create a list of European countries
+Europe = ["Russia", "Ukraine", "France", "Spain", "Sweden", "Norway", "Germany", "Finland", "Poland", "Italy", "Romania", "Belarus", "Kazakhstan", "Greece", "Bulgaria", "Iceland", "Hungary", "Portugal", "Austria", "Czechia", "Serbia", "Ireland", "Lithuania", "Latvia", "Croatia", "Bosnia-Herzegovina", "Slovakia", "Estonia", "Denmark", "Switzerland", "Netherlands", "Moldova", "Belgium", "Armenia", "Albania", "North-Macedonia", "Turkey", "Slovenia", "Montenegro", "Kosovo", "Cyprus", "Azerbaijan", "Luxembourg", "Georgia", "Andorra", "Malta", "Liechtenstein", "San_Marino", "Monaco", "Vatican_City", "United_Kingdom"]
+
