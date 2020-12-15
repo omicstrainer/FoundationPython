1d96e794bb943d56cd24f25c2d99211607890e7a
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 23:40:19 +0530

    Update list-example9.py

--- List-Python/list-example9.py
+++ List-Python/list-example9.py
@@ -10,3 +10,16 @@ Purpose - Example of Difference in Deletion
 ## Create a list of European countries
 Europe = ["Russia", "Ukraine", "France", "Spain", "Sweden", "Norway", "Germany", "Finland", "Poland", "Italy", "Romania", "Belarus", "Kazakhstan", "Greece", "Bulgaria", "Iceland", "Hungary", "Portugal", "Austria", "Czechia", "Serbia", "Ireland", "Lithuania", "Latvia", "Croatia", "Bosnia-Herzegovina", "Slovakia", "Estonia", "Denmark", "Switzerland", "Netherlands", "Moldova", "Belgium", "Armenia", "Albania", "North-Macedonia", "Turkey", "Slovenia", "Montenegro", "Kosovo", "Cyprus", "Azerbaijan", "Luxembourg", "Georgia", "Andorra", "Malta", "Liechtenstein", "San_Marino", "Monaco", "Vatican_City", "United_Kingdom"]
 
+print (Europe)
+
+Countries = Nations = Europe 
+
+Nations = []
+
+print(Nations)
+
+print(Europe)
+
+print(Countries)
+
+print(Countries[0:11])
