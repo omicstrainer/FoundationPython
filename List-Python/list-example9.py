07da2187e63213a6a63a0dd45ee908c5aa49c5ed
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 23:21:38 +0530

    Update list-example9.py

--- List-Python/list-example9.py
+++ List-Python/list-example9.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on TUE 15/12/2020 22:43
+File name: list-example8.py
+@author: Abhishek Kumar
+Purpose - Example of Deep copy - assignment of one list to another and then changes in one list will be NOT seen in other also.
+"""
+
+˚˚#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Wed Oct  7 13:22:12 2020
 
 @author: Abhishek Kumar
