0717edd97ba7c0f5730fc8c219abc20a6f1ae4fe
drabhishekkumar <draabhishekkumar@gmail.com>
2020-12-15 22:49:20 +0530

    Update list-example7.py

--- List-Python/list-example7.py
+++ List-Python/list-example7.py
@@ -8,6 +8,7 @@ Purpose - Example Shallow copy - assignment of one list to another and then chan
 """
 #Create list1
 list1 = [11, 22, 33, 44, 55, 66, 77]
+#print list1
 print (list1)
 list2 = list1
-print (list1)
+print (list2)
